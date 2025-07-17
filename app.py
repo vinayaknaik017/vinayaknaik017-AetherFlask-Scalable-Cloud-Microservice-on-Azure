3bda61e3e302aa3898f3ddc168e70f49686e9110
vinayaknaik017 <vinayak.is22@sahyadri.edu.in>
2025-07-17 22:35:40 +0530

    app contents

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World from Azure and VS Code!"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/pipeline")
+def pipeline():
+    return render_template("pipeline.html")
+
+@app.route("/logs")
+def logs():
+    return render_template("logs.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
     app.run()
